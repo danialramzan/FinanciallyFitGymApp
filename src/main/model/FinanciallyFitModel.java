31faf07e267ed8aac169fd7e3600389c1237a314
Danial Ramzan <danialramzan@gmail.com>
2023-10-13 16:35:58 -0700

    Phase 1.DONE

--- src/main/model/FinanciallyFitModel.java
+++ src/main/model/FinanciallyFitModel.java
@@ -2,6 +2,9 @@ package model;
 
 import java.util.List;
 
+/*
+ * Represents the Utility Code for FinanciallyFitUI
+ */
 public class FinanciallyFitModel {
 
     // Constructs a FinanciallyFitModel object.
